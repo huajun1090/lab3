72fa156f642ba9f2c7dba3e5a501582a781e9e3b
huajun <huajunchen2020@gmail.com>
2025-12-21 03:01:50 -0500

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Adam, Hua } from './Components/example'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 //import './App.css'
-import Pull from './Components/pull'
+import Pull from './Components/Pull'
 //import { fetchData } from './Components/fetchData'
 import fetchData from './Components/fetchData'
 import "@radix-ui/themes/styles.css";
